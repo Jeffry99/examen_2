83a9db9e170ada9cf99820c2ed9384208451e214
Jeffry99 <54681342+Jeffry99@users.noreply.github.com>
2020-10-29 15:12:45 -0600

    Correcciones

--- provincias/src/main/java/org/una/examen/provincias/entities/Canton.java
+++ provincias/src/main/java/org/una/examen/provincias/entities/Canton.java
@@ -6,6 +6,9 @@
 package org.una.examen.provincias.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,6 +16,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -43,4 +47,7 @@ public class Canton implements Serializable{
     @ManyToOne
     @JoinColumn(name="provincia", nullable = false)
     private Provincia provincia;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "canton") 
+    private List<Distrito> distritos= new ArrayList<>();
 }
